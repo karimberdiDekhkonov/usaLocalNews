74c98f541825ff09228edc78cd1936c7072a0143
karimberdiDekhkonov <karimberdidekhkonov0921@gmail.com>
2024-09-17 19:48:59 +0200

    RAPID API KEY IS CHANGED, CSV FILE IMPORTER RUNNER IS COMMENTED

--- src/main/java/com/example/usalocalnews/csv/CsvImportRunner.java
+++ src/main/java/com/example/usalocalnews/csv/CsvImportRunner.java
@@ -1,24 +1,24 @@
-package com.example.usalocalnews.csv;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-
-@Component
-public class CsvImportRunner implements CommandLineRunner {
-
-    @Autowired
-    private CsvService csvService;
-
-    @Autowired
-    private ImportService importService;
-
-    @Override
-    public void run(String... args) throws Exception {
-        String filePath = "./src/main/resources/uscities.csv";
-        List<String[]> records = csvService.readCsv(filePath);
-        importService.importCsvToDb(records);
-    }
-}
+//package com.example.usalocalnews.csv;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.List;
+//
+//@Component
+//public class CsvImportRunner implements CommandLineRunner {
+//
+//    @Autowired
+//    private CsvService csvService;
+//
+//    @Autowired
+//    private ImportService importService;
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        String filePath = "./src/main/resources/uscities.csv";
+//        List<String[]> records = csvService.readCsv(filePath);
+//        importService.importCsvToDb(records);
+//    }
+//}
